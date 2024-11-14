724c7b015a0d5ead96ad9bd5f5dbb7097ccb3e27
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2024-11-14 23:17:29 +0300

    create js file

---
+++ main.js
@@ -0,0 +1,11 @@
+let h1 = document.querySelector('h1');
+
+h1.onclick = function(){
+
+    h1.style.color = "red";
+    h1.style.margin = "0px";
+    h1.style.padding = "0px";
+    h1.style.transition = "1s";
+    h1.style.fontSize = "30px";
+}
+    
