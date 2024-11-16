6f1f9a45ece004c1856e488f1b989ddb87e85985
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2024-11-17 01:50:03 +0300

    finish events of creme brulee crad

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 let waffie = document.getElementById("waffie");
 
-let plas = document.createElement("span");
-plas.appendChild(document.createTextNode("+"));
-plas.style.padding = "5px";
+let waffieplas = document.createElement("span");
+waffieplas.appendChild(document.createTextNode("+"));
+waffieplas.style.padding = "5px";
 
-let NumberOfOrders = document.createElement("span");
-NumberOfOrders.appendChild(document.createTextNode(1));
+let waffieNumberOfOrders = document.createElement("span");
+waffieNumberOfOrders.appendChild(document.createTextNode(1));
 
-let minus = document.createElement("span");
-minus.appendChild(document.createTextNode("-"));
-minus.style.padding = "5px";
+let waffieminus = document.createElement("span");
+waffieminus.appendChild(document.createTextNode("-"));
+waffieminus.style.padding = "5px";
 
 waffie.onclick = function () {
   waffie.innerHTML = "";
@@ -22,34 +22,94 @@ waffie.onclick = function () {
   waffie.style.justifyContent = "space-between";
   waffie.style.alignItems = "center";
   waffie.style.padding = "0px";
-  waffie.appendChild(plas);
-  waffie.appendChild(NumberOfOrders);
-  waffie.appendChild(minus);
+  waffie.appendChild(waffieplas);
+  waffie.appendChild(waffieNumberOfOrders);
+  waffie.appendChild(waffieminus);
 };
 
-plas.onclick = function (e) {
+waffieplas.onclick = function (e) {
   e.stopPropagation();
-  NumberOfOrders.textContent = Number(NumberOfOrders.textContent) + 1;
+  waffieNumberOfOrders.textContent =
+    Number(waffieNumberOfOrders.textContent) + 1;
 };
 
-minus.onclick = function (e) {
-  NumberOfOrders.textContent -= 1;
+waffieminus.onclick = function (e) {
+  waffieNumberOfOrders.textContent -= 1;
   e.stopPropagation();
-  if (NumberOfOrders.textContent == "0") {
+  if (waffieNumberOfOrders.textContent == "0") {
     waffie.innerHTML = "";
-    let font = document.createElement("i");
-    font.className = "fa-solid fa-basket-shopping";
-    let textOfAdd = document.createElement("p");
-    let aa = document.createTextNode("add to cart");
-    textOfAdd.appendChild(aa);
-    textOfAdd.style.color = "black";
-    waffie.appendChild(font);
-    waffie.appendChild(textOfAdd);
+    let waffiefont = document.createElement("i");
+    waffiefont.className = "fa-solid fa-basket-shopping";
+    let waffietextOfAdd = document.createElement("p");
+    let waffiePtext = document.createTextNode("add to cart");
+    waffietextOfAdd.appendChild(waffiePtext);
+    waffietextOfAdd.style.color = "black";
+    waffie.appendChild(waffiefont);
+    waffie.appendChild(waffietextOfAdd);
     waffie.style.backgroundColor = "white";
     waffie.style.padding = "5px ";
     waffie.style.display = "flex";
     waffie.style.justifyContent = "space-around";
     waffie.style.alignItems = "center";
-    NumberOfOrders.textContent = Number(NumberOfOrders.textContent) + 1;
+    waffieNumberOfOrders.textContent =
+      Number(waffieNumberOfOrders.textContent) + 1;
+  }
+};
+
+let creme_brulee = document.getElementById("creme_brulee");
+
+let creme_bruleeplas = document.createElement("span");
+creme_bruleeplas.appendChild(document.createTextNode("+"));
+creme_bruleeplas.style.padding = "5px";
+
+let creme_bruleeNumberOfOrders = document.createElement("span");
+creme_bruleeNumberOfOrders.appendChild(document.createTextNode(1));
+
+let creme_bruleeminus = document.createElement("span");
+creme_bruleeminus.appendChild(document.createTextNode("-"));
+creme_bruleeminus.style.padding = "5px";
+
+creme_brulee.onclick = function () {
+  creme_brulee.innerHTML = "";
+  creme_brulee.style.fontSize = "13px";
+  creme_brulee.style.backgroundColor = "brown";
+  creme_brulee.style.color = "white";
+  creme_brulee.style.transition = "0s";
+  creme_brulee.style.width = "100px";
+  creme_brulee.style.display = "flex";
+  creme_brulee.style.justifyContent = "space-between";
+  creme_brulee.style.alignItems = "center";
+  creme_brulee.style.padding = "0px";
+  creme_brulee.appendChild(creme_bruleeplas);
+  creme_brulee.appendChild(creme_bruleeNumberOfOrders);
+  creme_brulee.appendChild(creme_bruleeminus);
+};
+
+creme_bruleeplas.onclick = function (e) {
+  e.stopPropagation();
+  creme_bruleeNumberOfOrders.textContent =
+    Number(creme_bruleeNumberOfOrders.textContent) + 1;
+};
+
+creme_bruleeminus.onclick = function (e) {
+  creme_bruleeNumberOfOrders.textContent -= 1;
+  e.stopPropagation();
+  if (creme_bruleeNumberOfOrders.textContent == "0") {
+    creme_brulee.innerHTML = "";
+    let creme_bruleefont = document.createElement("i");
+    creme_bruleefont.className = "fa-solid fa-basket-shopping";
+    let creme_bruleetextOfAdd = document.createElement("p");
+    let creme_bruleePtext = document.createTextNode("add to cart");
+    creme_bruleetextOfAdd.appendChild(creme_bruleePtext);
+    creme_bruleetextOfAdd.style.color = "black";
+    creme_brulee.appendChild(creme_bruleefont);
+    creme_brulee.appendChild(creme_bruleetextOfAdd);
+    creme_brulee.style.backgroundColor = "white";
+    creme_brulee.style.padding = "5px ";
+    creme_brulee.style.display = "flex";
+    creme_brulee.style.justifyContent = "space-around";
+    creme_brulee.style.alignItems = "center";
+    creme_bruleeNumberOfOrders.textContent =
+      Number(creme_bruleeNumberOfOrders.textContent) + 1;
   }
 };
