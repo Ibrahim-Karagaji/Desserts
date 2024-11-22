7126529a897d7b2bdaeac6504fb1f06aa989c8b5
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2024-11-22 22:17:16 +0300

    created function that changed display and color of the addtion elemnet when click it

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
-function addingplasMark(i) {
+function styleAfterAdd(i) {
+  i.style.backgroundColor = "purple";
+  i.style.display = "flex";
+  i.style.justifyContent = "justify-content: space-between";
+  i.style.padding = "5px";
+}
+
+function plasMark(i) {
   let plas = document.createTextNode("+");
   i.appendChild(plas);
 }
-function addingplasMark(i) {
+
+function muinsMark(i) {
   let muins = document.createTextNode("-");
   i.appendChild(muins);
 }
 
+function addingTheOrder(i) {
+  i.innerHTML = "";
+  styleAfterAdd(i);
+  let plas = document.createElement("span");
+  plasMark(plas);
+  let muins = document.createElement("span");
+  muinsMark(muins);
+  i.appendChild(plas);
+  i.appendChild(muins);
+}
+
+let i = document.getElementById("waffle-addtion");
+
+i.onclick = function () {
+  addingTheOrder(i);
+};
