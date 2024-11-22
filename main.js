86a4c18f1f86e25c9adfd20a12f10e5c36574006
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2024-11-22 22:34:56 +0300

    creatd functon of Number Of Orders that added

--- main.js
+++ main.js
@@ -1,33 +1,27 @@
 function styleAfterAdd(i) {
   i.style.backgroundColor = "purple";
   i.style.display = "flex";
-  i.style.justifyContent = "justify-content: space-between";
+  i.style.justifyContent = "space-between";
   i.style.padding = "5px";
+  i.style.border = "1px solid white";
+  i.style.width = "85px";
+  i.style.transition = "0s";
 }
 
 function plasMark(i) {
   let plas = document.createTextNode("+");
+  i.style.color = "white";
   i.appendChild(plas);
 }
 
 function muinsMark(i) {
   let muins = document.createTextNode("-");
+  i.style.color = "white";
   i.appendChild(muins);
 }
 
-function addingTheOrder(i) {
-  i.innerHTML = "";
-  styleAfterAdd(i);
-  let plas = document.createElement("span");
-  plasMark(plas);
-  let muins = document.createElement("span");
-  muinsMark(muins);
-  i.appendChild(plas);
-  i.appendChild(muins);
+function numberOfTheOrder(i) {
+  let Number = document.createTextNode("0");
+  i.style.color = "white";
+  i.appendChild(Number);
 }
-
-let i = document.getElementById("waffle-addtion");
-
-i.onclick = function () {
-  addingTheOrder(i);
-};
