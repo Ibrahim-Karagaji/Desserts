a3f3e954362b893c612e401fa72e228f49733552
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2024-11-22 23:05:18 +0300

    creat function return back the frist appearance of the Elemnet

--- main.js
+++ main.js
@@ -2,10 +2,25 @@ function styleAfterAdd(i) {
   i.style.backgroundColor = "purple";
   i.style.display = "flex";
   i.style.justifyContent = "space-between";
-  i.style.padding = "5px";
+  i.style.padding = "4px";
   i.style.border = "1px solid white";
   i.style.width = "85px";
   i.style.transition = "0s";
+}
+
+function backToStyleBeforeAdd(i) {
+  i.innerHTML = "";
+  i.style.display = "flex";
+  i.style.alignItems = "center";
+  i.style.backgroundColor = "white";
+  i.style.border = "1px solid purple";
+  let font = document.createElement("i");
+  font.className = "fa-solid fa-cart-plus";
+  i.appendChild(font);
+  let p = document.createElement("p");
+  let textP = document.createTextNode("add To Cart");
+  p.appendChild(textP);
+  i.appendChild(p);
 }
 
 function plasMark(i) {
@@ -21,7 +36,15 @@ function muinsMark(i) {
 }
 
 function numberOfTheOrder(i) {
-  let Number = document.createTextNode("0");
+  let Number = document.createTextNode("1");
   i.style.color = "white";
   i.appendChild(Number);
+}
+
+function addingTheOrder(i, p, n, m) {
+  i.innerHTML = "";
+  styleAfterAdd(i);
+  i.appendChild(p);
+  i.appendChild(n);
+  i.appendChild(m);
 }
