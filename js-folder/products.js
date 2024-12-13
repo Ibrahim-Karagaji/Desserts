c66e09e271a2a659d46f93363ec94f220b904a66
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2024-12-13 23:03:16 +0300

    created products file and put the values

---
+++ js-folder/products.js
@@ -0,0 +1,11 @@
+const products = [
+  { title: "Waffie", name: "Wasffie With Berries", price: 6.5 },
+  { title: "Creme Brulee", name: "Vanilla Bean Creme Brulee", price: 7.0 },
+  { title: "Macaron", name: "Macaron Mix Of Five", price: 8.0 },
+  { title: "Tiramisu", name: "Classic Tiramisu", price: 5.5 },
+  { title: "Baklava", name: "Baklava Pistachio", price: 4.4 },
+  { title: "Pie", name: "Lemon Meringue Pie", price: 5.5 },
+  { title: "Cake", name: "Red Velvet Cake", price: 4.5 },
+  { title: "Brownie", name: "Salted Caramel Brownie", price: 5.5 },
+  { title: "Panna", name: "Vanilla Panna Cotta", price: 6.5 },
+];
