c3c6b63fcb505f362923ca2b3efbaaa8952f13e1
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2024-12-14 23:58:09 +0300

    create an array has References of the Images

--- js-folder/add-item.js
+++ js-folder/add-item.js
@@ -1,3 +1,11 @@
-function CreateImage(e) {
-  e.src = "images-Folder/image-waffle-desktop.jpg";
-}
+const imgsReference = [
+  "images-Folder/image-waffle-desktop.jpg",
+  "images-Folder/image-creme-brulee-desktop.jpg",
+  "images-Folder/image-macaron-desktop.jpg",
+  "images-Folder/image-tiramisu-desktop.jpg",
+  "images-Folder/image-baklava-desktop.jpg",
+  "images-Folder/image-meringue-desktop.jpg",
+  "images-Folder/image-cake-desktop.jpg",
+  "images-Folder/image-brownie-desktop.jpg",
+  "images-Folder/image-panna-cotta-desktop.jpg",
+];
