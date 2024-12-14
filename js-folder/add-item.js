95ab2d34fa4b6c755357330e39dc913e3e77ebcb
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2024-12-15 00:15:03 +0300

    created function create's description of the card

--- js-folder/add-item.js
+++ js-folder/add-item.js
@@ -18,3 +18,23 @@ function addTocart(e) {
   let addP = document.createElement("p");
   e.appendChild(addP.appendChild(document.createTextNode("add To Cart")));
 }
+
+function descriptioning(e, container) {
+  e.id = "description";
+
+  let h3 = document.createElement("h3");
+  let h3Text = document.createTextNode(container.title);
+  h3.appendChild(h3Text);
+  e.appendChild(h3);
+
+  let span = document.createElement("span");
+  let spanText = document.createTextNode(container.name);
+  span.appendChild(spanText);
+  e.appendChild(span);
+
+  let descriptionP = document.createElement("p");
+  let descriptionPText = document.createTextNode(container.price.toFixed(2));
+  descriptionP.appendChild(descriptionPText);
+  e.appendChild(descriptionP);
+}
+
