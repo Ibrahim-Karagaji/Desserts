d0c52e873b2af3665d68f8e3e967ee5494e2ad4d
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2024-12-15 00:09:08 +0300

    created function creates add to cart part at the card

--- js-folder/add-item.js
+++ js-folder/add-item.js
@@ -9,3 +9,12 @@ const imgsReference = [
   "images-Folder/image-brownie-desktop.jpg",
   "images-Folder/image-panna-cotta-desktop.jpg",
 ];
+
+function addTocart(e) {
+  e.id = "adding";
+  let icon = document.createElement("i");
+  icon.className = "fa-solid fa-basket-shopping";
+  e.appendChild(icon);
+  let addP = document.createElement("p");
+  e.appendChild(addP.appendChild(document.createTextNode("add To Cart")));
+}
