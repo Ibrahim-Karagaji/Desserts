e16cb725dcce9f86a8ef9d5b0fd40ca1eafbe663
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2024-12-15 22:32:14 +0300

    change gap between the cards and preparings card

--- js-folder/preparing.js
+++ js-folder/preparing.js
@@ -1,5 +1,7 @@
 function preparStyle(e) {
+  e.style.display = "grid";
   e.style.padding = "10px";
+  e.style.gap = "5px";
   e.style.height = "fit-content";
   e.style.backgroundColor = "#eee";
   e.style.borderRadius = "10px";
@@ -8,6 +10,23 @@ function preparStyle(e) {
 function totlesCounter(e, ordersTotle) {
   cartText = document.createTextNode(`Add To Cart (${ordersTotle.totle})`);
   e.style.color = "#f26314b8";
-  e.style.fontWeight = "600";
+  e.style.fontWeight = "700";
   e.appendChild(cartText);
+}
+
+function preparingImg(e, ordersTotle) {
+  e.src = ordersTotle.img;
+  e.style.width = "300px";
+  e.style.height = "280px";
+  e.style.margin = "0px auto";
+}
+
+function prepar(pareant, ordersTotle) {
+  preparStyle(pareant);
+  let cart = document.createElement("p");
+  totlesCounter(cart, ordersTotle);
+  let img = document.createElement("img");
+  preparingImg(img, ordersTotle);
+  pareant.appendChild(cart);
+  pareant.appendChild(img);
 }
