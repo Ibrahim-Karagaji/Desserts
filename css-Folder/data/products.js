3cb5f0334c44aaf5bc5fd30a4f7068a16f8f63b1
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2024-12-30 22:47:42 +0300

    create file has Products Data

---
+++ css-Folder/data/products.js
@@ -0,0 +1,11 @@
+const product = [
+  { title: "Waffie", name: "Waffie With Berries", price: 6.50 },
+  { title: "Creme", name: "Vanilla Bean Creme Brulee", price: 7.00 },
+  { title: "Macaron", name: "Macaron Mix Of Five", price: 8.00 },
+  { title: "Tiramisu", name: "Classic Tiramisu", price: 5.50 },
+  { title: "Baklava", name: "Pisachio Baklava", price: 4.00 },
+  { title: "Pie", name: "Lemon Meringue Pie", price: 5.50 },
+  { title: "Cake", name: "Red Velvet Cake", price: 4.50 },
+  { title: "Brownie", name: "Salted Caramel Brownie", price: 5.50 },
+  { title: "Panna Cotta", name: "Vanilla Panna Cotta", price: 6.50 },
+];
