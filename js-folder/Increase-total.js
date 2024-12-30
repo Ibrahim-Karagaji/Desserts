3c496f91847a6b58170864b22a210bebbfb1b1d0
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2024-12-30 23:36:36 +0300

    add img's reference at products data

--- js-folder/Increase-total.js
+++
@@ -1,8 +0,0 @@
-function IncreaseTotal(e) {
-  e.textContent = e.textContent.replace(/\d+/, (match) => parseInt(match) + 1);
-}
-
-function ReduceTotal(e) {
-  e.textContent = e.textContent.replace(/\d+/, (match) => parseInt(match) - 1);
-}
-
--- js-folder/ordersTotle.js
+++
@@ -1,4 +0,0 @@
-const ordersTotle = {
-  totle: 0.00,
-  img: "images-Folder/illustration-empty-cart.svg",
-};
