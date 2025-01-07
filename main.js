b0648af7c8e05bc3af0408ff7b7bd9c4d53e551c
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-01-07 03:34:27 +0300

    git elements pareant at js mian file

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+const elementParent = document.querySelector(".parent");
 const products_cards = document.querySelector(".products-cards");
 const shoping_totle = document.querySelector(".shoping-totle");
+const Comaplteorders = document.querySelector("dialog");
 
 createCard(product, products_cards);
 prepareProducts(shoping_totle);
